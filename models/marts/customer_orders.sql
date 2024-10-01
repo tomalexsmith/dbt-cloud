5a0428a2656656f096d20810b94a04e60e235a16
Thomas Alexander Smith <thomas.smith@theinformationlab.co.uk>
2024-10-01 12:39:11 +0000

    add order_total and lifetime value columns to customer_orders and dim_customers

--- models/marts/customer_orders.sql
+++ models/marts/customer_orders.sql
@@ -8,6 +8,7 @@ select
     customer_id,
     min(order_date) as first_order_date,
     max(order_date) as most_recent_order_date,
-    count(order_id) as number_of_orders
+    count(order_id) as number_of_orders,
+    sum(order_total) as lifetime_value
 from orders
 group by customer_id
--- models/staging/stg_jaffle_shop__orders.sql
+++ models/staging/stg_jaffle_shop__orders.sql
@@ -2,5 +2,6 @@ select
     id as order_id,
     customer as customer_id,
     ordered_at as order_date,
-    store_id
+    store_id,
+    order_total
 from RAW.JAFFLE_SHOP.ORDERS
